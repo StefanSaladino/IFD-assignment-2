c6639193b96da038145c7d95b58214e49cb59ba8
StefanSaladino <122896468+StefanSaladino@users.noreply.github.com>
2023-07-22 14:44:41 -0400

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   let currentSlide = 0;
+  //slides are each element in the "slides" class
   const slides = document.getElementsByClassName("slides");
+  //lines are each element with a "line" class
   const lines = document.getElementsByClassName("line");
+  //first element called hamburger-icon is assigned the hamburgerIcon variable
   const hamburgerIcon = document.querySelector(".hamburger-icon");
+  //first element called menu-items is assigned the hamburgerIcon variable
   const menuItems = document.querySelector(".menu-items");
   
 
-
+  //when the page loads, the first line (on the left) will be highlighted
   lines[0].classList.add("highlight");
 
   // Add event listener to each line
+  // when a line is clicked, the showSlide function is called, set to the corresponding slide
   for (let i = 0; i < lines.length; i++) {
     lines[i].addEventListener("click", function () {
       showSlide(i);
     });
   }
 
+  //defining showSlide function
   function showSlide(n) {
     //if n is less than 0, it resets the value to the last slide
     //if n is higher than the last index place, it sets the slide back to index 0
@@ -33,12 +39,15 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     //the currentSlide is set to a display of flex to remain congruent with the CSS
+    //ensuring only one slide is displayed at a time
     slides[currentSlide].style.display = "flex";
 
+    //removing the highlight class from each line so multiple lines arent highlighted
     for (let i = 0; i < lines.length; i++) {
       lines[i].classList.remove("highlight");
     }
 
+    //the line corresponding to the current slide is highlighted
     lines[currentSlide].classList.add("highlight");
   }
 
@@ -65,6 +74,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const prevArrow = document.querySelector(".arrow-left");
   const nextArrow = document.querySelector(".arrow-right");
 
+  //click events to each arrow calling a function
   prevArrow.addEventListener("click", previousSlide);
   nextArrow.addEventListener("click", nextSlide);
 });
